9033beb88f3a34c1359cc45295192797457638a7
Viterbo <viter.rod@gmail.com>
2024-05-05 21:19:53 -0300

    fix href and website title

--- examples/demo/vue.config.js
+++ examples/demo/vue.config.js
@@ -2,5 +2,8 @@ module.exports = {
     devServer: {
         port: 8081
     },
-    outputDir: 'build'
+    outputDir: 'build',
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/telos-cloud-js/'
+        : '/'
 }
